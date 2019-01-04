bd62b09f077cc0923b3ee9fdb480f2630ecf784e
Jonas <Jonas@JL.fritz.box>
2019-01-04 16:46:40 +0100

    Fehler bei den Treiber. Connection nicht herstellbar.

--- BA_GenerateCorpData/src/DB_Connect.java
+++ BA_GenerateCorpData/src/DB_Connect.java
@@ -1,9 +1,62 @@
+import java.sql.*;
+
 
 public class DB_Connect {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	final String hostname = "192.168.178.34";
+	final String port = "3306";
+	final String dbname = "test";
+	final String user = "root";
+	final String password = "mond2395";
+	
+	public DB_Connect() {
+		
 	}
-
+	
+	public Connection erstelleVerbindung() {
+		
+		Connection conn = null;
+		
+		
+		try {
+			System.out.println("Treiber laden...");
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		}
+		catch (Exception e) {
+			System.out.println("Unable to load driver.");
+			e.printStackTrace();
+		}
+		try {
+			System.out.println("Verbindung aufbauen...");
+			String url = "jdbc:mysql://"+hostname+":"+port+"/"+dbname;
+			conn = DriverManager.getConnection(url, user, password);
+		}
+		catch (SQLException sqle) {
+			System.out.println("SQLException: "+ sqle.getMessage());
+			System.out.println("SQLStat: "+ sqle.getSQLState());
+			System.out.println("VendorError: "+ sqle.getErrorCode());
+			sqle.printStackTrace();
+		}
+		
+		
+		if(conn != null)
+			return conn;
+		else
+			return null;
+		
+	}
+	
+	public boolean testTreiber() {
+		
+		try {
+			System.out.println("Treiber laden...");
+			Class.forName("org.hsqldb.jdbcDriverr").newInstance();
+		}
+		catch (Exception e) {
+			System.out.println("Unable to load driver.");
+			e.printStackTrace();
+		}
+		return true;
+	}
+	
 }
--- BA_GenerateCorpData/src/Main.java
+++ BA_GenerateCorpData/src/Main.java
@@ -1,4 +1,29 @@
+import java.sql.*;
 
 public class Main {
 
+	public static void main(String[] args) {
+		
+		boolean testCon = db_connectTEST(); //--> funktioniert nicht
+
+		
+	}
+	
+	private static boolean db_connectTEST() {
+		
+		Connection conn = null;
+		DB_Connect dbconn = new DB_Connect();
+		
+		conn = dbconn.erstelleVerbindung();
+		
+		
+		if(conn == null) {
+			System.out.println("Fehler");
+			return false;
+		}else {
+			System.out.println("Erfolgreich");
+			return true;
+		}
+	}
+	
 }
