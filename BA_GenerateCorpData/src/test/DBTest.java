f542bd64150d66d14edf567284814c4d49dfef79
Jonas <Jonas@JL>
2019-01-05 23:58:18 +0100

    Hier wird DB besser getestet.. muss noch Programmiert werden, sodass man nicht immer acht mal das gleiche schreiben muss

--- BA_GenerateCorpData/src/test/DBTest.java
+++ BA_GenerateCorpData/src/test/DBTest.java
@@ -2,4 +2,24 @@ package test;
 
 public class DBTest {
 
+	static {
+		try {
+			System.out.println("Treiber wird geladen...");
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		}
+		catch (Exception e) {
+			System.out.println("Fehler beim Laden des Treibers..");
+			e.printStackTrace();
+			System.exit(0);;
+		}
+		System.out.println("Treiber erfolgreich geladen.");
+		
+	}
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
